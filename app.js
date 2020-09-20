9094adfa138682d4dea487d71dd6556fdca2a645
AbdulHaadi26 <abdulhaadirao@gmail.com>
2020-09-20 17:23:55 +0500

    Added mongoose

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 var express = require('express')
 var bodyParser = require('body-parser')
 var app = express()
@@ -7,21 +8,30 @@ var mongoose = require('mongoose');
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 
-var messages = [
-    {name: 'Tim', message: 'Hi'},
-    {name: 'Jane', message: 'Hello'}
-]
-
-app.get('/messages', (req, res) =>{
-    res.send(messages)
+var msgModel = mongoose.model('messages', {
+    name: { type: String },
+    message: { type: String }
 })
 
-app.post('/messages', (req, res) =>{
-    messages.push(req.body)
-    io.emit('message', req.body)
-    res.sendStatus(200)
+app.get('/messages', async (req, res) => {
+    var data = await msgModel.find();
+    if (!data) res.sendStatus(500);
+    res.send(data);
+})
+
+app.post('/messages', async (req, res) => {
+    try {
+        var message = new msgModel(req.body);
+        var message = await message.save();
+        if (!message) res.sendStatus(500);
+        io.emit('message', req.body)
+        res.sendStatus(200);
+    } catch (e) {
+        res.sendStatus(500);
+        console.log(e.message);
+    }
 })
 
 io.on('connection', (socket) => {
@@ -29,7 +39,7 @@ io.on('connection', (socket) => {
 })
 
 //MongoDB Connection String
-const mongoURI = `mongodb+srv://${process.env.NAME}:${process.env.PASS}@dev-02rqv.mongodb.net/development?retryWrites=true&w=majority`;
+const mongoURI = `mongodb+srv://${process.env.NAME}:${process.env.PASS}@dev-02rqv.mongodb.net/chat?retryWrites=true&w=majority`;
 
 //Connect to Mongodb
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true }, async function (err) {
